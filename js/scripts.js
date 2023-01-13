2575826b53aee97bd7f3dc1f00ccca024b7f9925
Kai Clausen <kaiclausen123@gmail.com>
2023-01-13 10:45:36 -0800

    Create variables for the form selection values to reside in. Create submit button.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function add(number1, number2, number3, number4, number5) {
+  return number1 + number2 + number3 + number4 + number5;
+}
+
+function handleSelect(event) {
+  event.preventDefault();
+  const input1 = parseInt(document.getElementById("select-beverage").value);
+  const input2 = parseInt(document.getElementById("select-car").value);
+  const input3 = parseInt(document.getElementById("select-animal ").value);
+  const input4 = parseInt(document.getElementById("select-shoe"));
+  const input5 = parseInt(document.getElementById("select-event"));
+}
