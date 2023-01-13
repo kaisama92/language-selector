9d279dd3601c0a8e636e9ae1092978437ecb6ca4
Kai Clausen <kaiclausen123@gmail.com>
2023-01-13 11:26:10 -0800

    WIP - debugging if then statements.

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,23 @@ function handleSelect(event) {
   event.preventDefault();
   const input1 = parseInt(document.getElementById("select-beverage").value);
   const input2 = parseInt(document.getElementById("select-car").value);
-  const input3 = parseInt(document.getElementById("select-animal ").value);
+  const input3 = parseInt(document.getElementById("select-animal").value);
   const input4 = parseInt(document.getElementById("select-shoe"));
   const input5 = parseInt(document.getElementById("select-event"));
   
-  let output = document.getElementById("output")
+  let output;
+  //  = document.getElementById("output")
   let totalValue = parseInt(add(input1, input2, input3, input4, input5));
 
-  if (totalValue <= 5) {
+  if (parseInt(totalValue) <= 5) {
     output = "JavaScript"
-  } else if (totalValue > 5 && totalValue <= 10) {
+    console.log("What da hell")
+  } else if (parseInt(totalValue) > 5 && totalValue <= 10) {
     output = "Python"
-  } else if (totalValue > 10) {
+  } else if ("totalValue" > 10) {
     output = "C++"
   }
+  document.getElementById("output").innerText = output
 }
 
 window.addEventListener("load", function() {
